65d3241ee3583e884989b2a71310cd7cb8621343
Omar Y. Alvarado <omarjesith@gmail.com>
2025-11-26 17:49:26 -0500

    feat: centralize environment file configuration, enhance CI workflow with dynamic env creation, and add Django model migrations.

--- app/core/settings.py
+++ app/core/settings.py
@@ -12,11 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1").split(",")
-
 
 # Application definition
 
